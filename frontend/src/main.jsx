1c9cbc104bd8e580e818d2e552adf2ed7b41654d
Murugapz <samuruimakes248@gmail.com>
2026-06-03 22:10:29 +0800

    feat: add auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AuthProvider } from './store/AuthContext';
+import { LoginGate } from './components/LoginGate';
 import './styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <LoginGate>
+        <App />
+      </LoginGate>
+    </AuthProvider>
   </React.StrictMode>
 );
